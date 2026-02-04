722b8effaee8299d8c97e2befc8b727ebb3eb6f8
LEPHUOCHAO <hao_dpm235507@student.agu.edu.vn>
2026-02-04 19:44:47 +0700

    Add files via upload

---
+++ BTTH04/QLNhanVien/BusinessLogicLayer/ChucVu_BUS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using DataTransferObject;
+
+namespace BusinessLogicLayer
+{
+    public class ChucVu_BUS
+    {
+        public static List<ChucVu_DTO> LayChucVu()
+        {
+            return ChucVu_DAO.LayChucVu();
+        }
+    }
+}
---
+++ BTTH04/QLNhanVien/BusinessLogicLayer/NhanVien_BUS.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer;
+using DataTransferObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class NhanVien_BUS
+    {
+        public static List<NhanVien_DTO> LayNhanVien()
+        {
+            return NhanVien_DAO.LayNhanVien();
+        }
+    }
+}
